8779020b5bf05272d05652801e37bf8202157580
aruytehno <aruytehno@yandex.ru>
2024-04-17 15:10:59 +0300

    Добавил второй вариант генератора паролей

--- Python/password_generator.py
+++ Python/password_generator.py
@@ -1,4 +1,11 @@
 from itertools import chain
 import random
+import string
+
+'''
+Генератор паролей на списковых включениях в одну строку
+'''
 
 print(''.join(str(x) for x in [chr(random.choice(list(chain(range(65, 91), range(97, 123), range(48, 58))))) for x in range(20)]))
+# short
+print(''.join(str(x) for x in [random.choice(string.ascii_letters + string.digits) for x in range(20)]))
